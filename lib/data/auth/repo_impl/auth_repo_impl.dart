ddf257e1d93a18fd732b4e8938dfa470a25b438a
Ahmed-Abdelghany-11 <anofal786@gmail.com>
2025-06-19 04:22:12 +0300

    feat(auth): implement login functionality with local and remote data sources

--- lib/data/auth/repo_impl/auth_repo_impl.dart
+++ lib/data/auth/repo_impl/auth_repo_impl.dart
@@ -1,6 +1,31 @@
+import 'package:fitness_app/core/utils/datasource_excution/api_manager.dart';
+import 'package:fitness_app/core/utils/datasource_excution/api_result.dart';
+import 'package:fitness_app/data/auth/data_source/contract/auth_local_data_source.dart';
+import 'package:fitness_app/data/auth/models/request/login_request_dto.dart';
+import 'package:fitness_app/data/auth/models/response/login_response_dto.dart';
 import 'package:injectable/injectable.dart';
 
+import '../../../core/utils/constants.dart';
 import '../../../domain/auth/repo/auth_repo.dart';
+import '../data_source/contract/auth_remote_data_source.dart';
 
 @Injectable(as: AuthRepo)
-class AuthRepoImpl implements AuthRepo {}
+class AuthRepoImpl implements AuthRepo {
+  final ApiManager _apiManager;
+  final AuthRemoteDataSource _authRemoteDataSource;
+  final AuthLocalDataSource _authLocalDataSource;
+  AuthRepoImpl(
+    this._authRemoteDataSource,
+    this._authLocalDataSource,
+    this._apiManager,
+  );
+  @override
+  Future<Result<LoginResponseDto>> login(LoginRequestDto request) async {
+    var response = await _apiManager.execute<LoginResponseDto>(() async {
+      var response = await _authRemoteDataSource.login(request);
+      _authLocalDataSource.saveToken(Constants.token, response.token ?? "");
+      return response;
+    });
+    return response;
+  }
+}
--- lib/domain/auth/use_case/login_use_case.dart
+++ lib/domain/auth/use_case/login_use_case.dart
@@ -1,1 +1,17 @@
+import 'package:fitness_app/core/utils/datasource_excution/api_result.dart';
+import 'package:injectable/injectable.dart';
 
+import '../../../data/auth/models/request/login_request_dto.dart';
+import '../../../data/auth/models/response/login_response_dto.dart';
+import '../repo/auth_repo.dart';
+
+@injectable
+class LoginUseCase {
+  final AuthRepo _authRepo;
+
+  LoginUseCase(this._authRepo);
+
+  Future<Result<LoginResponseDto>> call(LoginRequestDto request) async {
+    return await _authRepo.login(request);
+  }
+}
