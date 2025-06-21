118f6b8874162eecb02e54ab589620903a7257f7
Eslam <eslamm378@gmail.com>
2025-06-21 09:46:37 +0300

    feat(workouts): Implement Retrofit client for workouts and exercises API

---
+++ lib/data/workouts/api/workouts_retrofit_client.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:fitness_app/data/workouts/models/exercises_response_dto.dart';
+import 'package:fitness_app/data/workouts/models/workouts_response_dto.dart';
+import 'package:injectable/injectable.dart';
+import 'package:retrofit/retrofit.dart';
+
+import '../../../core/utils/datasource_excution/api_constants.dart';
+
+part 'workouts_retrofit_client.g.dart';
+
+@injectable
+@RestApi(baseUrl: ApiConstants.baseUrl)
+abstract class WorkoutsRetrofitClient {
+  @factoryMethod
+  factory WorkoutsRetrofitClient(Dio dio) = _WorkoutsRetrofitClient;
+
+  @GET(ApiConstants.allMuscles)
+  Future<WorkoutsResponseDto> getAllMuscles();
+
+  @GET(ApiConstants.allExercises)
+  Future<ExercisesResponseDto> getAllExercises();
+}
