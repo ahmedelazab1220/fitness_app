ad00a0bc778df34ca087da061b418a5c72533243
Eslam <eslamm378@gmail.com>
2025-07-02 05:29:55 +0300

    feat(auth): Add user entity and profile data retrieval use case

---
+++ lib/domain/auth/entity/user_entity.dart
@@ -0,0 +1,25 @@
+class UserEntity {
+  final String? firstName;
+  final String? lastName;
+  final String? email;
+  final String? gender;
+  final int? age;
+  final int? weight;
+  final int? height;
+  final String? activityLevel;
+  final String? goal;
+  final String? photo;
+
+  UserEntity({
+    this.firstName,
+    this.lastName,
+    this.email,
+    this.gender,
+    this.age,
+    this.weight,
+    this.height,
+    this.activityLevel,
+    this.goal,
+    this.photo,
+  });
+}
--- lib/domain/auth/repo/auth_repo.dart
+++ lib/domain/auth/repo/auth_repo.dart
@@ -1,1 +1,6 @@
-abstract class AuthRepo {}
+import 'package:fitness_app/core/utils/datasource_excution/api_result.dart';
+import 'package:fitness_app/domain/auth/entity/user_entity.dart';
+
+abstract class AuthRepo {
+  Future<Result<UserEntity>> getProfileData();
+}
