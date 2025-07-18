7753ea030d47659b113c39027e35d675ce50ba71
Eslam <eslamm378@gmail.com>
2025-07-18 09:32:11 +0300

    feat(auth): add change password method to remote data source

--- lib/data/auth/data_source/contract/auth_remote_data_source.dart
+++ lib/data/auth/data_source/contract/auth_remote_data_source.dart
@@ -1,5 +1,7 @@
+import 'package:fitness_app/data/auth/models/change_password/response/change_password_response_dto.dart';
 import 'package:fitness_app/data/auth/models/response/register_response_dto.dart';
 
+import '../../models/change_password/request/change_password_request_dto.dart';
 import '../../models/request/register_request_dto.dart';
 import '../../models/forget_password/request/forget_password_request_dto.dart';
 import '../../models/forget_password/response/forget_password_response_dto.dart';
@@ -26,5 +28,9 @@ abstract interface class AuthRemoteDataSource {
 
   Future<ResetPasswordResponseDto> resetPassword(
     ResetPasswordRequestDto request,
+  );
+
+  Future<ChangePasswordResponseDto> changePassword(
+    ChangePasswordRequestDto request,
   );
 }
